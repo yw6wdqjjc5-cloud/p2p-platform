c69645ee7b8eaa7d3667c74ecccedb482231950b
Артём Степанов <artemstepanov@MacBook-Air-Artem.local>
2025-11-21 20:55:28 +0300

    Add fallback for SUPABASE_SERVICE_ROLE_KEY

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,11 +4,9 @@ const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
   'https://tknysxsctsgmtkmputhg.supabase.co';
 
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-
-if (!supabaseServiceRoleKey) {
-  throw new Error('Missing SUPABASE_SERVICE_ROLE_KEY environment variable.');
-}
+const supabaseServiceRoleKey =
+  process.env.SUPABASE_SERVICE_ROLE_KEY ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrbnlzeHNjdHNnbXRrbXB1dGhnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzY1OTI5NywiZXhwIjoyMDc5MjM1Mjk3fQ.8MUwyfF1WzbxlQf_rvif6VKrSaSNhfjjxIHNplwg-Yk';
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey, {
   auth: {
